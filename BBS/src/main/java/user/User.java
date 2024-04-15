980a847f2e9a386f3243a711162cebf6dbd95b35
Park_HeonKwon <cookingman1209@gmail.com>
2024-04-15 11:29:42 +0900

    데이터베스 & 테이블 만들기

---
+++ BBS/src/main/java/user/User.java
@@ -0,0 +1,42 @@
+package user;
+
+public class User {
+
+		private String ID;
+		private String Password;
+		private String Name;
+		private String Gender;
+		private String Email;
+		
+		
+		public String getID() {
+			return ID;
+		}
+		public void setID(String iD) {
+			ID = iD;
+		}
+		public String getPassword() {
+			return Password;
+		}
+		public void setPassword(String password) {
+			Password = password;
+		}
+		public String getName() {
+			return Name;
+		}
+		public void setName(String name) {
+			Name = name;
+		}
+		public String getGender() {
+			return Gender;
+		}
+		public void setGender(String gender) {
+			Gender = gender;
+		}
+		public String getEmail() {
+			return Email;
+		}
+		public void setEmail(String email) {
+			Email = email;
+		}
+}
